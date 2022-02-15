189155e9588beacd60e27c482f3d4498cdbf125a
Ian Findlay <code@findlayian.com>
2022-02-15 12:42:21 +0000

    Remove unneeded newline

--- models/articles.models.js
+++ models/articles.models.js
@@ -9,7 +9,6 @@ exports.selectArticleById = (articleId) => {
       JOIN comments ON articles.article_id = comments.article_id
       WHERE articles.article_id = $1
       GROUP BY articles.article_id;
-
       `,
       [articleId]
     )
